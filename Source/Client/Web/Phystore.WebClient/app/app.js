dcdcb9ea55031ee476d1b45b3c5db5684aba59b9
Konstantin86 <kostyan22@gmail.com>
2015-02-08 19:00:01 +0200

    Added base client infrastructure, added libs: angular, jquery, bootstrap, angular-strap and angular-motion.css

---
+++ Source/Client/Web/Phystore.WebClient/app/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('PhystoreApp', ['ngRoute', 'ngAnimate', 'mgcrea.ngStrap', 'mgcrea.ngStrap.modal']);
+
+app.config(function ($routeProvider) {
+
+    //$routeProvider.when("/progress", {
+    //    controller: "progressController",
+    //    templateUrl: "/app/views/progress.html"
+    //});
+
+    //$routeProvider.when("/home", {
+    //    controller: "homeController",
+    //    templateUrl: "/app/views/home.html"
+    //});
+
+    //$routeProvider.otherwise({ redirectTo: "/home" });
+});
