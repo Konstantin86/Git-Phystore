b9b00812e98048be0f2170e9d3eaf6f315c62523
Konstantin86 <kostyan22@gmail.com>
2015-02-17 00:31:08 +0200

    Added carousel to the home page with starting images

--- Source/Client/Web/Phystore.WebClient/app/controllers/homeController.js
+++ Source/Client/Web/Phystore.WebClient/app/controllers/homeController.js
@@ -4,32 +4,59 @@
 
     var hostUri = appConfig.getInstance().getHostUri();
 
-    //$scope.myInterval = 5000;
+    var slides = $scope.slides = [];
 
-    //var slides = $scope.slides = [];
+    slides.push({
+        image: hostUri + 'content/images/1-set-your-goals.jpg',
+        text: 'Set your goals...'
+    });
 
-    //Aivazovsky_Park.jpg
-    //slides.push({
-    //    image: hostUri + 'content/images/1.jpg',
-    //    text: 'aaa'
-    //});
+    slides.push({
+        image: hostUri + 'content/images/2-create-workout-plan.jpg',
+        text: 'Create workout plan...'
+    });
 
-    //slides.push({
-    //    image: hostUri + 'content/images/0001.jpg',
-    //    text: 'bbb'
-    //});
+    slides.push({
+        image: hostUri + 'content/images/3-work-hard.jpg',
+        text: 'Work hard...'
+    });
 
+    slides.push({
+        image: hostUri + 'content/images/4-take-suggestions-from-community.jpg',
+        text: 'Suggest with community...'
+    });
+
+    slides.push({
+        image: hostUri + 'content/images/5-do-something-every-day.jpg',
+        text: 'Do something every day...'
+    });
+
+    slides.push({
+        image: hostUri + 'content/images/6-achieve-your-goals.jpg',
+        text: 'Achieve your goals...'
+    });
+
+    slides.push({
+        image: hostUri + 'content/images/7-share-your-success.jpg',
+        text: 'Share your success...'
+    });
+
+    slides.push({
+        image: hostUri + 'content/images/8-observe-places.jpg',
+        text: 'Observe Places...'
+    });
+    
         $scope.myInterval = 5000;
-        var slides = $scope.slides = [];
-        $scope.addSlide = function () {
-            var newWidth = 600 + slides.length + 1;
-            slides.push({
-                image: 'http://placekitten.com/' + newWidth + '/300',
-                text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
-                  ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-            });
-        };
-        for (var i = 0; i < 4; i++) {
-            $scope.addSlide();
-        }
+        //var slides = $scope.slides = [];
+        //$scope.addSlide = function () {
+        //    var newWidth = 600 + slides.length + 1;
+        //    slides.push({
+        //        image: 'http://placekitten.com/' + newWidth + '/300',
+        //        text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
+        //          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        //    });
+        //};
+        //for (var i = 0; i < 4; i++) {
+        //    $scope.addSlide();
+        //}
 });
