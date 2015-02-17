f0282df7779a4a194b40c01b8bc9c118fafb8754
Konstantin86 <kostyan22@gmail.com>
2015-02-17 12:40:56 +0200

    Added ng-flow file uploader lib, removed redundand images

--- Source/Client/Web/Phystore.WebClient/app/app.js
+++ Source/Client/Web/Phystore.WebClient/app/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('PhystoreApp', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.tpls', 'LocalStorageModule', 'mgcrea.ngStrap', 'mgcrea.ngStrap.helpers.dateParser', 'angular-loading-bar']);
+﻿var app = angular.module('PhystoreApp', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.tpls', 'flow', 'LocalStorageModule', 'mgcrea.ngStrap', 'mgcrea.ngStrap.helpers.dateParser', 'angular-loading-bar']);
 
 app.config(function ($routeProvider) {
     // TODO create directive for disabling ng animation (used for carousel) https://github.com/angular-ui/bootstrap/issues/1350
