5e7571b198180d998a52928f87e39a1d23b5f75f
Konstantin86 <kostyan22@gmail.com>
2015-02-17 23:13:00 +0200

    Added base File Upload infrastructure

---
+++ Source/Api/Phystore.WebApi/Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Newtonsoft.Json.Linq;
+
+namespace Phystore.WebApi.Controllers
+{
+  [RoutePrefix("api/image")]
+  public class ImageController : ApiController
+  {
+    [Route("test", Name = "test")]
+    [HttpPost]
+    [ResponseType(typeof(JObject))]
+    public async Task<IHttpActionResult> Postimage()
+    {
+      // Check if the request contains multipart/form-data.
+      if (!Request.Content.IsMimeMultipartContent())
+      {
+        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+      }
+
+      string root = HttpContext.Current.Server.MapPath("~/App_Data");
+      var provider = new MultipartFormDataStreamProvider(root);
+
+      try
+      {
+        // Read the form data.
+        await Request.Content.ReadAsMultipartAsync(provider);
+
+        // This illustrates how to get the file names.
+        foreach (MultipartFileData file in provider.FileData)
+        {
+          //Console.WriteLine(file.Headers.ContentDisposition.FileName);
+          //Trace.WriteLine("Server file path: " + file.LocalFileName);
+          if (File.Exists(Path.Combine(root, "test.jpg")))
+            File.Delete(Path.Combine(root, "test.jpg"));
+
+          File.Move(file.LocalFileName, Path.Combine(root, "test.jpg"));
+          return Ok();
+        }
+      }
+      catch (System.Exception e)
+      {
+      }
+      return Ok();
+    }
+  }
+}
--- Source/Client/Web/Phystore.WebClient/app/app.js
+++ Source/Client/Web/Phystore.WebClient/app/app.js
@@ -48,3 +48,19 @@ app.run(['authService', function (authService) {
     authService.init();
 }]);
 
+app.config(['flowFactoryProvider', function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+        target: appConfig.getInstance().getServiceUri() + 'api/image/test',
+        permanentErrors: [404, 500, 501],
+        maxChunkRetries: 1,
+        chunkRetryInterval: 5000,
+        simultaneousUploads: 4,
+        singleFile: true
+    };
+    flowFactoryProvider.on('catchAll', function (event) {
+        console.log('catchAll', arguments);
+    });
+    // Can be used with different implementations of Flow.js
+    // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
+
